ce59443394592294055a7e3d562e9f52dad16991
Carlos Peralta <carlos9917@gmail.com>
2020-02-25 16:50:17 +0000

    added some details

--- process_odb.py
+++ process_odb.py
@@ -1,7 +1,8 @@
 import pandas as pd
-data0=pd.read_csv('obs_1_11_1.dat',sep=' ')
-Rast=data['an_depar@body']*data['fg_depar@body']
 import math
+#data for control run
+data0=pd.read_csv('mbr000/odb_ccma/CCMA/obs_1_11_1.dat',sep=' ')
+Rast=data['an_depar@body']*data['fg_depar@body']
 Rast_mean=Rast.mean()
 error = math.sqrt(Rast_mean)
 fg2=data['fg_depar@body']*data['fg_depar@body'] # (O - fg)^2
@@ -11,6 +12,11 @@ obs_error_mean = data['obs_error@errstat'].mean()
 
 mems=[str(i).zfill(3) for i in range(1,10)]
 fg_ctrl_mean = data0['fg_depar@body'].mean()
+#statid@hdr varno@body vertco_reference_1@body obsvalue@body an_depar@body fg_depar@body obs_error@errstat
+
+#varno@body vertco_reference_1@body obsvalue@body an_depar@body fg_depar@body obs_error@errstat
+
+for station in 
 for mem in mems:
     ifile=os.path.join('mbr'+mem+'/odb_ccma/CCMA/','mbr'+mem+'_obs_1_11_1.dat')
     data = pd.read_csv(ifile,sep=' ')
