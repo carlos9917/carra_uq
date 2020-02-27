3c3c2feec0262f8fe813b08681feb1f167bb9487
Carlos Peralta <carlos9917@gmail.com>
2020-02-27 08:44:43 +0100

    updated process_odb.py to pile up data for all init times for a given station

--- process_odb.py
+++ process_odb.py
@@ -43,29 +43,41 @@ def calc_one_init():
             #fg_ctrl_mem = (fg_ctrl_mean - mean()*fg_mean)**2
         diff_total = diff2_station/len(mems)    
         print("Total difference for station %d: %g"%(station,diff_total))
-def calc_all_init():
+
+def calc_all_init(fdate):
+
     init_times=[str(i).zfill(2) for i in range(0,22,3)]
+    mems=[str(i).zfill(3) for i in range(1,10)]
+
+    #for k,station in enumerate(data_ref['statid@hdr']):
+    #    print("Going through station %d"%station)
+    #    diff2_station = 0
+    #for init in init_times:
+    diff2_init={} #store all the differences for a particular init time
     for init in init_times:
-        data0=pd.read_csv(os.path.join(init,'mbr000/odb_ccma/CCMA/mbr000_obs_1_11_1.dat'),sep=' ')
-        mems=[str(i).zfill(3) for i in range(1,10)]
-        fg_ctrl_mean = data0['fg_depar@body'].mean()
-        for k,station in enumerate(data0['statid@hdr']):
-            print("Going through station %d"%station)
+        yyyymmddii='/'.join([fdate,init])
+        data_ref=pd.read_csv(os.path.join(yyyymmddii,'mbr000/odb_ccma/CCMA/mbr000_obs_1_11_1.dat'),sep=' ')   
+        for k,station in enumerate(data_ref['statid@hdr']):
+            diff2_station=0
             fg_ctrl = data0['fg_depar@body'].values[k]
-            diff2_station = 0
             for mem in mems:
-                ifile=os.path.join('mbr'+mem+'/odb_ccma/CCMA/','mbr'+mem+'_obs_1_11_1.dat')
+                ifile=os.path.join(yyyymmddii,'mbr'+mem+'/odb_ccma/CCMA/mbr'+mem+'_obs_1_11_1.dat')
                 data = pd.read_csv(ifile,sep=' ')
                 #select only the station in member which matches station in control run list
                 mem_sel = data[data['statid@hdr'] == station]
                 if not mem_sel.empty:
                     print("Station found in member %s"%mem)
                     fg_mem = mem_sel['fg_depar@body']
-                    diff2 = (fg_ctrl - fg_mem)**2
-                    diff2_station += diff2
-            diff_total = diff2_station/len(mems)    
-            print("Total difference for station %d: %g"%(station,diff_total))
-def calc_all_init():
+                    diff2_station += (fg_ctrl - fg_mem)**2
+            #diff2_init[station+'_'+init] = diff2_station/len(mems)    
+            if init == '00': # create dataframe
+                diff2_init=pd.Dataframe({'station':station,'init':init,'diff2':diff2_station/len(mems)})
+            else:
+                diff2_init.append({'station':station,'init':init,'diff2':diff2_station/len(mems)})
+               
+    #print("Total difference for station %d at init time %s: %g"%(station,init,diff_total))
+    #finally calculate mean of all init times for all stations that 
+    #are present during the whole set of init times
 
 if __name__=='__main__':
-
+    calc_all_init('2012/07/01')
