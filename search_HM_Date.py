69a560829d7236c1ada4aeb655db69227254941f
Carlos Peralta <carlos9917@gmail.com>
2020-03-01 17:39:55 +0000

    search_HM_Date.py: print only the unique keys

--- search_HM_Date.py
+++ search_HM_Date.py
@@ -1,18 +1,21 @@
 #Crude script to generate the sql commands to call
 #all different variables available in the HM_Date files
 import sys
+import os
 from itertools import islice
 
 def search_all_init(fdate):
     '''
-    Search in the HM_Date* file for this date
+    Search all Obstype/Codetype pairs in the HM_Date* file for this date.
+    Print only non-repeated values
     '''
     init_times=[str(i).zfill(2) for i in range(0,22,3)]
     obs_code={}
     for init in init_times:
         yyyymmddii='/'.join([fdate,init])
+        print("Date %s"%yyyymmddii)
 
-        searchfile = open(os.path.join(yyyymmddii,"HM_Date_"+yyyymmddii.replace('/','')+".html", "r")
+        searchfile = open(os.path.join(yyyymmddii,"HM_Date_"+yyyymmddii.replace('/','')+".html"), "r")
         for line in searchfile:
             if ('Obstype     ') in line: 
                 #the following line will store the next two lines in a list
@@ -26,6 +29,12 @@ def search_all_init(fdate):
                     codetype=next_two[1].split()[1]
                     #print("Obstype: %s Codetype: %s"%(obstype,codetype))
                     obs_code[obstype]=codetype
+    uniqueKeys = set(obs_code.keys())
+    obs_code_clean={}
+    for key in uniqueKeys:
+        obs_code_clean[key] = obs_code[key]
+
+    return obs_code_clean
 
 def search_single():
     obs_code={}
@@ -72,6 +81,7 @@ if __name__=='__main__':
                         required=True)
 
     args = parser.parse_args()
-    #grab the observation codes for all files in this date
-    obs_code = search_all_init(fdate)
+    #grab the observation codesarg for all files in this date
+    obs_code = search_all_init(args.date)
+    print(obs_code)
 
