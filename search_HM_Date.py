b9f986838cb3636e584184485d437af7e6cfd3ff
Carlos Peralta <carlos9917@gmail.com>
2020-03-02 13:42:23 +0000

    Changed search_HM_Date.py to locate all pairs of Obstype/Codetype

--- search_HM_Date.py
+++ search_HM_Date.py
@@ -3,8 +3,23 @@
 import sys
 import os
 from itertools import islice
+import numpy as np
 
 def search_all_init(fdate):
+    '''
+    Search in all the HM_Date* file for this date
+    '''
+    init_times=[str(i).zfill(2) for i in range(0,22,3)]
+    obs_types={} #store obs_types for each file
+    for init in init_times:
+        yyyymmddii='/'.join([fdate,init])
+        print("searching %s"%yyyymmddii)
+        fname=os.path.join(yyyymmddii,"HM_Date_"+yyyymmddii.replace('/','')+".html")
+        searchfile = open(fname, "r")
+        obs_types[fname.replace(".html","")]=search_single(fname)
+    return obs_types
+
+def search_all_init_old(fdate):
     '''
     Search all Obstype/Codetype pairs in the HM_Date* file for this date.
     Print only non-repeated values
@@ -36,7 +51,7 @@ def search_all_init(fdate):
 
     return obs_code_clean
 
-def search_single():
+def search_single_old():
     obs_code={}
     searchfile = open("./uq_data/HM_Date_2012070100.html", "r")
     for line in searchfile:
@@ -61,6 +76,45 @@ def search_single():
     #print(obs_code)
     return obs_code
 
+def search_single(sfile):
+    end_line='----------   ---------------------------   --------'
+    obs_types={}
+    searchfile = open(sfile,"r") #"./uq_data/HM_Date_2012070100.html", "r")
+    inRecordingMode=False
+    lines_saved=[]
+    for line in searchfile:
+        if ('Obstype     ') in line:
+            #print("found one")
+            next_two=list(islice(searchfile,2))
+            #print(next_two)
+            if 'Codetype' in next_two[1]:
+                #print("creating array")
+                #save the next lines until it hits the end_line
+                inRecordingMode=True
+                lines_saved=[]
+        elif end_line in line:
+            inRecordingMode=False
+
+        if inRecordingMode:
+            #print("recording")
+            lines_saved.append(line)
+            if len(lines_saved) == 1:
+                lines_saved.append(next_two[0])
+                lines_saved.append(next_two[1])
+            #the following line will store the next two lines in a list
+            #the first element will be the line ------
+        if not inRecordingMode and len(lines_saved) > 0:
+            #print("checking lines")
+            for l in lines_saved:
+                if 'Obstype' in l: # create array to store all Codetype(s) for this Obstype
+                    obstype=l.split()[1]
+                    obs_types[obstype] = np.array([])
+                if 'Codetype' in l:  #store the Codetype(s) for this Obstype
+                    codetype=l.split()[1]
+                    obs_types[obstype]=np.append(obs_types[obstype],codetype)
+    searchfile.close()
+    return obs_types
+
 #commands to call
 def run_sql():
     import subprocess
@@ -82,6 +136,8 @@ if __name__=='__main__':
 
     args = parser.parse_args()
     #grab the observation codesarg for all files in this date
-    obs_code = search_all_init(args.date)
-    print(obs_code)
+    obs_types = search_all_init(args.date)
+    print("Obstype/Codetype pairs found in each file")
+    for key in obs_types.keys():
+        print(obs_types[key])
 
